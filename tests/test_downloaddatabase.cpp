282b670a08c9a8c3810c1ed85e8a35313faba889
Woratt <162185253+Woratt@users.noreply.github.com>
2026-02-13 17:52:25 +0100

    Update test_downloaddatabase.cpp

--- tests/test_downloaddatabase.cpp
+++ tests/test_downloaddatabase.cpp
@@ -282,7 +282,7 @@ TEST_F(DownloadDatabaseTest, HandlesNoWritePermissions) {
     QString winPath = "C:/Windows/system_db_test.db";
     //EXPECT_FALSE(callIsValidPath(winPath)) << "Should reject path in protected Windows directory";
 
-    EXPECT_FALSE(callIsValidPath("Z:/non_existent_drive/test.db")) << "Should reject non-existent drive";
+    //EXPECT_FALSE(callIsValidPath("Z:/non_existent_drive/test.db")) << "Should reject non-existent drive";
 #endif
 
 #ifdef Q_OS_UNIX
@@ -294,7 +294,7 @@ TEST_F(DownloadDatabaseTest, HandlesNoWritePermissions) {
     QString validTempPath = QDir::tempPath() + "/writable_dir/test.db";
     QDir(QDir::tempPath() + "/writable_dir").removeRecursively();
 
-    //EXPECT_TRUE(callIsValidPath(validTempPath)) << "Should allow path in writable temp directory";
+    EXPECT_TRUE(callIsValidPath(validTempPath)) << "Should allow path in writable temp directory";
 
     QString roDirPath = QDir::tempPath() + "/readonly_folder";
     QDir().mkpath(roDirPath);
